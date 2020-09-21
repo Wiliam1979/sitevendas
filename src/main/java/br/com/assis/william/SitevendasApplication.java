c928396ddd9fed0509cfd6358ac7634713091d91
William Assis <willifran@gmail.com>
2020-09-21 16:37:43 -0300

    Projeto criado

---
+++ src/main/java/br/com/assis/william/SitevendasApplication.java
@@ -0,0 +1,13 @@
+package br.com.assis.william;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitevendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitevendasApplication.class, args);
+	}
+
+}

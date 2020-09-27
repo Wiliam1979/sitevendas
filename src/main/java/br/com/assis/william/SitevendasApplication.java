77a0fb5fe74a5d0d6b7417bb6d3890293259b8d2
William Assis <willifran@gmail.com>
2020-09-27 14:07:50 -0300

    criando operação de instanciação

--- src/main/java/br/com/assis/william/SitevendasApplication.java
+++ src/main/java/br/com/assis/william/SitevendasApplication.java
@@ -1,13 +1,32 @@
 package br.com.assis.william;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SitevendasApplication {
+import br.com.assis.william.domain.Categoria;
+import br.com.assis.william.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class SitevendasApplication implements CommandLineRunner {
+
+	  @Autowired
+	 private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SitevendasApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }

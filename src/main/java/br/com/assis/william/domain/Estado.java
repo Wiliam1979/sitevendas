25508195dda40ffc7e2f1a3e574ba5053660313f
William Assis <willifran@gmail.com>
2020-10-02 06:17:12 -0300

    Endpoint/cliente/{id} disponivel

--- src/main/java/br/com/assis/william/domain/Estado.java
+++ src/main/java/br/com/assis/william/domain/Estado.java
@@ -10,6 +10,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Estado implements Serializable {
 	
@@ -27,7 +29,7 @@ private String nome;
 public Estado() {
 	
 }
- 
+ @JsonBackReference
  @OneToMany(mappedBy="estado")
 private List<Cidade>cidades = new ArrayList<>();
 
---
+++ src/main/java/br/com/assis/william/services/ClienteService.java
@@ -0,0 +1,27 @@
+package br.com.assis.william.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.assis.william.domain.Categoria;
+import br.com.assis.william.domain.Cliente;
+import br.com.assis.william.repositories.ClienteRepository;
+import br.com.assis.william.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo; 
+	
+	public Cliente find(Integer id) {
+		Optional<Cliente>obj = repo.findById(id);
+		
+		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado! Id: " +id+ ", Tipo:" +Cliente.class.getName()));
+			
+		
+	}
+
+}

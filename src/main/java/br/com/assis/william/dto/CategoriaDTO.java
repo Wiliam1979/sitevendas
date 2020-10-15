fd6adb549caf1360408d0d6ff79f130ef2ba48c7
William Assis <willifran@gmail.com>
2020-10-15 06:04:24 -0300

    listando todas as categorias com DTO

---
+++ src/main/java/br/com/assis/william/dto/CategoriaDTO.java
@@ -0,0 +1,43 @@
+package br.com.assis.william.dto;
+
+import java.io.Serializable;
+
+import br.com.assis.william.domain.Categoria;
+
+public class CategoriaDTO implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	
+	private String nome;
+	
+	public CategoriaDTO() {
+		
+	}
+	
+	public CategoriaDTO(Categoria obj) {
+		id = obj.getId();
+		nome = obj.getNome();
+		
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
--- src/main/java/br/com/assis/william/resources/CategoriaResource.java
+++ src/main/java/br/com/assis/william/resources/CategoriaResource.java
@@ -1,6 +1,9 @@
 package br.com.assis.william.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.assis.william.domain.Categoria;
+import br.com.assis.william.dto.CategoriaDTO;
 import br.com.assis.william.services.CategoriaService;
 
 @RestController
@@ -58,4 +62,17 @@ public class CategoriaResource {
 	 
 	 
 		}
+		@RequestMapping( method=RequestMethod.GET)
+		public ResponseEntity<List<CategoriaDTO>> findAll() {	
+			
+			List<Categoria> list = service.findAll();
+			 List<CategoriaDTO> listDto = list.stream().map(obj-> new CategoriaDTO(obj)).collect(Collectors.toList());
+			return ResponseEntity.ok().body(listDto);
+			
+			
+			
+			
+		}
+		
+		
 }

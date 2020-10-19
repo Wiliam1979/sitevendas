d38a3dab346af84e1973582fdea8f294cee7ba98
William Assis <willifran@gmail.com>
2020-10-19 16:33:37 -0300

    PUT, DELETE E GET para cliente

--- src/main/java/br/com/assis/william/services/ClienteService.java
+++ src/main/java/br/com/assis/william/services/ClienteService.java
@@ -1,12 +1,19 @@
 package br.com.assis.william.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.assis.william.domain.Cliente;
+import br.com.assis.william.dto.ClienteDTO;
 import br.com.assis.william.repositories.ClienteRepository;
+import br.com.assis.william.services.exceptions.DataIntegrityException;
 import br.com.assis.william.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -22,5 +29,42 @@ public class ClienteService {
 			
 		
 	}
+	
+	  public Cliente update(Cliente obj) {
+	    	Cliente newObj = find(obj.getId());
+	    	updateDate(newObj, obj);
+	    	return repo.save(newObj);
+	    }
+	    
+	    public void delete(Integer id) {
+	    	try {   
+	    	
+	    	find(id);
+	    	repo.deleteById(id);
+	    	} catch(DataIntegrityViolationException e) {
+	    		throw new DataIntegrityException("Não e possivel excluir porque há entidade relacionadas");
+	    		
+	    	}
+	    	
+	    }
+		public List<Cliente> findAll() {
+			
+			return repo.findAll();
+		}
+		
+		public Page<Cliente>findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+			PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),
+					orderBy);
+			return repo.findAll(pageRequest);
+		}
+		
+		public Cliente fromDTO(ClienteDTO objDto) {
+			return new Cliente(objDto.getId(),objDto.getNome(), objDto.getEmail(),null, null);
+		}
+		
+		private void updateDate(Cliente newObj, Cliente obj) {
+			newObj.setNome(obj.getNome());
+			newObj.setEmail(obj.getEmail());
+		}
 
 }
